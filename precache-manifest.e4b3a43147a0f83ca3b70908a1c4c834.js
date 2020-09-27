d696df215333b7572bd4efdb8d5b1d9f024a262f
tbdoan <tbdoan@ucsd.edu>
2020-09-27 16:17:49 -0700

    Updates

---
+++ precache-manifest.e4b3a43147a0f83ca3b70908a1c4c834.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2cbab70ce29bb7e8bd2bed72d672ebd",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "6a55cc7cd35b7e85e79e",
+    "url": "/home/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "63ebd2d33ebd154a075f",
+    "url": "/home/static/css/main.8e59e3f5.chunk.css"
+  },
+  {
+    "revision": "6a55cc7cd35b7e85e79e",
+    "url": "/home/static/js/2.8f592bbd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/home/static/js/2.8f592bbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63ebd2d33ebd154a075f",
+    "url": "/home/static/js/main.8b9acc6f.chunk.js"
+  },
+  {
+    "revision": "21db2f8c6e3113e64fa6",
+    "url": "/home/static/js/runtime-main.b83746fc.js"
+  },
+  {
+    "revision": "b039a816eec6fdc2343e7ea4038e9db3",
+    "url": "/home/static/media/pfp.b039a816.png"
+  }
+]);
